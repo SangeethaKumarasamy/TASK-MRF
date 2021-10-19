80e3074d2f5b8e94ce6ce4a029814af7785d8157
Sangeetha Kumarasamy <89238309+SangeethaKumarasamy@users.noreply.github.com>
2021-10-19 11:45:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -108,11 +108,80 @@ function intersection(a,b)
 {
       return a.filter((inter_sec)=> parseInt(b.filter((intersec)=> inter_sec==intersec)))
 }
-console.log(intersection(arr1,arr2))
+console.log(intersection(arr1,arr2));
 
 //uniq
 function uniq(a)
 {
       return a.filter((data,index)=> a.indexOf(data)===index) 
 }
-console.log(uniq(arr1))
+console.log(uniq(arr1));
+
+
+
+//without
+function without(a,...b)
+{
+    return a.filter((data)=>(!b.includes(data)));
+}
+console.log(without(arr1,7,9))
+
+
+
+//groupby 
+let arr3=["one","two","three","four","six","seven"]
+
+function groupbyLength(array)
+{
+   let obj={};
+  let lengtharray=array.map(group_by=>group_by.length)
+  let result= lengtharray.filter((data,index)=> lengtharray.indexOf(data)===index) 
+  for(let i=0;i<result.length;i++)
+  {
+      obj[result[i]]=array.filter(gb=> result[i]==gb.length);
+  }
+  return obj;
+
+} 
+console.log(groupbyLength(arr3));
+
+//deposit task
+class Account{
+  constructor(name,accType,accNo,balance){
+    this.name=name;
+    this.account_type=accType;
+    this.account_number=accNo;
+    this.balance=balance;
+  }
+  getBalance() {
+    return "Your balance is ₹" + this.balance;
+  }
+
+  withdraw(withdrawAmt) {
+    if (withdrawAmt <= this.balance) {
+      this.balance = this.balance - withdrawAmt;
+      return this.getBalance();
+    } else {
+      return "Insufficent funds. " + this.getBalance();
+    }
+  }
+  //task
+ deposit(depositAmt) {
+   
+      this.balance = this.balance +depositAmt;
+   return this.getBalance();
+   
+      
+ }
+}
+const sourabh=new Account("Sourabh","Savings",11111,25_000)
+const lokesh=new Account("Lokesh","current",11112,10_000)
+const surya=new Account("Surya","Savings",11113,1_00_000)
+const asritha=new Account("Asritha","DEMAT",11114,20_000)
+console.log(sourabh,lokesh,surya,asritha);
+console.log(surya.getBalance());
+console.log(surya.withdraw(2_00_000));
+console.log(asritha.deposit(10_000));
+
+
+
